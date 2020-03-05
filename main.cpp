582f478b2a74b7f3b15f641ede1d6b1148e19464
YouKnowMario <55323338+YouKnowMario@users.noreply.github.com>
2020-03-05 16:07:40 +0200

    Folder Decryption and Encryption is done

--- main.cpp
+++ main.cpp
@@ -22,13 +22,12 @@ void printHelp()
 	std::cout << "for more info visit the docs - www.exmple.com" << std::endl;
 	std::cout << "-h or --help		print this message" << std::endl;
 	std::cout << "-e or --encrypt		encrypts the input file to the output file" << std::endl;
-	std::cout << "-d or --decrypt		decrypts the input encrypted file" << std::endl;
+	std::cout << "-d or --decrypt		decrypts the input encrypted file or directory" << std::endl;
 	std::cout << "-E			encrypts the input file and deletes it" << std::endl;
-	std::cout << "-D			decrypts the input file and deletes it" << std::endl;
+	std::cout << "-D			decrypts the input file or directory and deletes it" << std::endl;
 	std::cout << "-ed			encrypts the input directory to the output file" << std::endl;
-	std::cout << "-dd			decrypts the input encrypted directory file" << std::endl;
 	std::cout << "-ED			encrypts the input directory to the output file and deletes it" << std::endl;
-	std::cout << "-DD			decrypts the input encrypted directory file and deletes it" << std::endl;
+
 
 }
 
@@ -46,19 +45,6 @@ void printHelp()
 // fileSize | fileData | the data of the file
 //
 
-void test(std::string filename)
-{
-	std::ifstream rf(filename, std::ios::binary);
-	if (!rf) {
-		std::cout << "Cannot open file!" << std::endl;
-		return;
-	}
-	rf.seekg(37, std::ios::beg);
-	unsigned te;
-	rf.read((char*)&te, sizeof(unsigned));
-	std::cout << te << std::endl;
-	rf.close();
-}
 
 void encryptFile(std::string input, std::string output, std::string password, const int& mode)
 {
@@ -123,40 +109,76 @@ void encryptFile(std::string input, std::string output, std::string password, co
 
 void decryptFile(std::string input, std::string output, std::string password, const int& mode)
 {
-	
-	std::ifstream rf(input, std::ios::binary);
+	std::ifstream rf(input, std::ios::binary); // open the input file for read
 	if (!rf) {
 		std::cout << "Cannot open file!" << std::endl;
 		return;
 	}
+	std::vector<std::string> files;
+	char* format = new char[4];
 	
-	std::string fileName;
-	unsigned nameLength;
-	char temp;
-	rf.read((char*)&nameLength, sizeof(unsigned)); // read the length of the file name from the file
-	std::vector<char> tempNameArray;
-	tempNameArray.resize(nameLength);
-	rf.read((char*)&tempNameArray[0], nameLength * sizeof(char)); // read the name of the file from the file
-	fileName.assign(tempNameArray.begin(), tempNameArray.end());
-	std::ofstream wf(fileName, std::ios::binary);
-	if (!wf) {
-		std::cout << "Cannot open file for writing!" << std::endl;
-		return;
-	}
-	while (rf.read(&temp, 1)) // reuse
+	rf.read(format, sizeof(char) * 4);  // read the format
+	unsigned NameLength;
+	std::string name;
+	rf.read((char*)&NameLength, sizeof(unsigned));  // read the name length
+	name.resize(NameLength);
+	rf.read(&name[0], NameLength);  // read the name
+	//std::string name = tempName;
+	std::cout << name << std::endl;
+	if (std::filesystem::exists(name))
 	{
-		temp++;
-		wf.write(&temp, 1);
+		char answer;
+		std::cout << "\'" << name << "\'" << " is already exists. Overwrite? [y/n] ";
+		std::cin >> answer;
+		if (answer == 'y')
+		{
+			std::filesystem::remove(name); // remove the directory
+		}
+		else
+			std::cout << "Not overwriting - exists" << std::endl;
 	}
-	wf.close();
-	rf.close();
+
+	if (!strcmp(format, "dir ")) // the encryption is of directory
+	{
+		std::filesystem::create_directory(name); // create the directory
+		std::cout << "decrypting directory" << std::endl;
+		goto clean;
+	}
+	else if (strcmp(format, "file")) // the encryption is of file
+	{
+		std::ofstream wf(name, std::ios::binary); // open the output file to write
+		if (!wf) {
+			std::cout << "Cannot open file for writing!" << std::endl;
+			goto clean;
+		}
+		unsigned fileSize;
+		char byte;
+		rf.read((char*)&fileSize, sizeof(unsigned)); // read the file size
+		while (rf.read(&byte, 1))
+		{
+			byte++;
+			wf.write(&byte, 1);
+		}
+		wf.close();
+		rf.close();
+		goto clean;
+	}
+	else // wrong encryption
+	{
+		std::cout << "wrong file encryption" << std::endl;
+		goto clean;
+	}
+clean:
+
+	delete[] format;
+	return;
 
 }
 #ifdef _DEBUG // if debug
 int main()
 {
 	int argc = 5;
-	const char* argv[5] = { "encrypter", "-ed", "test", "lol", "dire" };
+	const char* argv[5] = { "encrypter", "-d", "encryptedImage", "lol", "hello_world2.png" };
 #else 
 
 int main(int argc, char* argv[])
@@ -178,17 +200,13 @@ int main(int argc, char* argv[])
 	else if (!strcmp(argv[1], "--decrypt") || !strcmp(argv[1], "-d"))
 		decryptFile(argv[2], output, argv[3], 0);
 	else if (!strcmp(argv[1], "-D"))
-		encryptFile(argv[2], output, argv[3], 1);
-	else if (!strcmp(argv[1], "-E"))
 		decryptFile(argv[2], output, argv[3], 1);
+	else if (!strcmp(argv[1], "-E"))
+		encryptFile(argv[2], output, argv[3], 1);
 	else if (!strcmp(argv[1], "-ed"))
 		encryptFile(argv[2], output, argv[3], 2);
-	else if (!strcmp(argv[1], "-dd"))
-		decryptFile(argv[2], output, argv[3], 2);
 	else if (!strcmp(argv[1], "-ED"))
 		encryptFile(argv[2], output, argv[3], 3);
-	else if (!strcmp(argv[1], "-DD"))
-		decryptFile(argv[2], output, argv[3], 3);
 	else
 		std::cout << "wrong parameter" << std::endl;
 
