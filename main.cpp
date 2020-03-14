d8c97630f75254e667a8ad8b65d075645ffe06df
YouKnowMario <55323338+YouKnowMario@users.noreply.github.com>
2020-03-14 13:10:38 +0200

    File and Directory Encryption and Decryption with RC4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <filesystem>
+#include <stdio.h>
+#include "RC4Enc.h"
 
 void printHelp()
 {
@@ -45,9 +50,34 @@ void printHelp()
 // fileSize | fileData | the data of the file
 //
 
+bool ifExist(const char* p_name)
+{
+	if (std::filesystem::exists(p_name))
+	{
+		char answer;
+		std::cout << "\'" << p_name << "\'" << " is already exists. Overwrite? [y/n] ";
+		std::cin >> answer;
+		if (answer == 'y')
+		{
+			std::filesystem::remove_all(p_name);
+			return true;
+		}
+		else
+		{
+			std::cout << "Not overwriting - exists" << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
 
 void encryptFile(std::string input, std::string output, std::string password, const int& mode)
 {
+
+	if (output == "")
+		output = "encrypted" + input;
+	if (!ifExist(output.c_str()))
+		return;
 	std::ofstream wf(output, std::ios::binary); // open the output file for write
 
 	std::vector<std::string> files;
@@ -55,7 +85,9 @@ void encryptFile(std::string input, std::string output, std::string password, co
 	if (mode == 0 || mode == 1)
 	{
 		files.push_back(input);
-		wf.write("file", sizeof(char) * 4);  // write the format
+		const char* c_format = RC4Enc::encrypt("file", password.c_str(), sizeof(char) * 4, password.size()); // encrypt the format
+		wf.write(c_format, sizeof(char) * 4); // write the encrypted format
+		delete[] c_format;
 	}
 	else if (mode == 2 || mode == 3)
 	{
@@ -63,19 +95,26 @@ void encryptFile(std::string input, std::string output, std::string password, co
 		{
 			files.push_back(name.path().string());
 		}
-		unsigned i = files.size();
-		wf.write("dir ", sizeof(char) * 4); // write the format
-		wf.write((char*)&i, sizeof(unsigned)); // write the fileNum
-		i = input.size();
-		wf.write((char*)&i, sizeof(unsigned)); // write the direcNameSize
-		wf.write(input.c_str(), i * sizeof(char)); // write the direcName
+		unsigned int filesNum = files.size();
+		const char* c_format = RC4Enc::encrypt("dir ", password.c_str(), sizeof(char) * 4, password.size()); // encrypt the format
+		wf.write(c_format, sizeof(char) * 4); // write the encrypted format
+		const char* c_filesNum = RC4Enc::encrypt((char*)&filesNum, password.c_str(), sizeof(unsigned), password.size()); // encrypt the file number
+		wf.write(c_filesNum, sizeof(unsigned)); // write the encrypted file number
+		filesNum = input.size(); // reuse the variable
+
+		const char* c_direcNameSize = RC4Enc::encrypt((char*)&filesNum/*direcNameSize*/, password.c_str(), sizeof(unsigned), password.size()); // encrypt the direcNameSize
+		wf.write(c_direcNameSize, sizeof(unsigned)); // write the encrypted direcNameSize
+		const char* c_direcName = RC4Enc::encrypt(input.c_str() , password.c_str(), filesNum/*direcNameSize*/, password.size()); // encrypt the direcName
+		wf.write(c_direcName, filesNum/*direcNameSize*/ * sizeof(char)); // write the encrypted direcName
+		delete[] c_format;
+		delete[] c_filesNum;
+		delete[] c_direcNameSize;
+		delete[] c_direcName;
 	}
-	if (output == "")
-		output = "encryptedFile";
+	std::string fileName;
 	for (std::string filename : files) // write the data
 	{
-		unsigned fileSize;
-		unsigned filenamelength;
+		fileName = filename.substr(filename.find_first_of('\\') + 1);
 		std::ifstream rf(filename, std::ios::binary); // open the file to encrypt
 		if (!rf) { // check if exist
 			std::cout << "Cannot open file!" << std::endl;
@@ -86,25 +125,51 @@ void encryptFile(std::string input, std::string output, std::string password, co
 			std::cout << "file is too big (only files up to 4 gb)" << std::endl;
 			return;
 		}
+		unsigned fileSize;
+		unsigned filenamelength;
 		fileSize = std::filesystem::file_size(filename);
-		std::cout << fileSize << std::endl;
-		filenamelength = filename.length();
+		filenamelength = fileName.length();
 		// write the data to the file
-		wf.write((char*)&filenamelength, sizeof(unsigned)); // write the length of the name
-		wf.write(filename.c_str(), filenamelength * sizeof(char)); // write the name
-		wf.write((char*)&fileSize, sizeof(unsigned)); // write the file size
-		// code for now (i will add encryption)
-		char temp;
-		while (rf.read(&temp, sizeof(char))) // write byte by byte to byte to the file
+		
+		const char* c_filenamelength = RC4Enc::encrypt((char*)&filenamelength, password.c_str(), sizeof(unsigned), password.size()); // encrypt the name size
+		wf.write(c_filenamelength, sizeof(unsigned)); // write the encrypted name size
+		delete[] c_filenamelength;
+		const char* c_filename = RC4Enc::encrypt(fileName.c_str(), password.c_str(), filenamelength, password.size()); // encrypt the file name
+		wf.write(c_filename, filenamelength); // write the encrypted file name
+		delete[] c_filename;
+		const char* c_fileSize = RC4Enc::encrypt((char*)&fileSize, password.c_str(), sizeof(unsigned), password.size()); // encrypt the file size
+		wf.write(c_fileSize, sizeof(unsigned)); // write the encrypted file size
+		delete[] c_fileSize;
+		
+		unsigned times = fileSize / 500;
+		const char* c_bytes;
+		char* bytes = new char[500];
+		for (unsigned i = 0; i < times; i++)
 		{
-			temp--;
-			wf.write(&temp, sizeof(char));
+			rf.read(bytes, sizeof(char) * 500);
+			c_bytes = RC4Enc::encrypt(bytes, password.c_str(), sizeof(char) * 500, password.size());
+			wf.write(c_bytes, sizeof(char) * 500);
+			delete[] c_bytes;
 		}
+
+		unsigned remainingBytes = fileSize % 500;
+		if (remainingBytes > 0)
+		{
+			delete[] bytes; // delete the bytes for reuse
+			bytes = new char[remainingBytes];
+			rf.read(bytes, sizeof(char) * remainingBytes);
+			c_bytes = RC4Enc::encrypt(bytes, password.c_str(), sizeof(char) * remainingBytes, password.size());
+			wf.write(c_bytes, sizeof(char) * remainingBytes);
+			delete[] c_bytes;
+		}
+		
+		// cleanup:
+		delete[] bytes;
 		rf.close(); // close the file
+		
 	}
-	
-	wf.close(); // closing the file
-	
+
+	wf.close(); // close the file
 }
 
 void decryptFile(std::string input, std::string output, std::string password, const int& mode)
@@ -114,71 +179,126 @@ void decryptFile(std::string input, std::string output, std::string password, co
 		std::cout << "Cannot open file!" << std::endl;
 		return;
 	}
-	std::vector<std::string> files;
-	char* format = new char[4];
-	
-	rf.read(format, sizeof(char) * 4);  // read the format
-	unsigned NameLength;
-	std::string name;
-	rf.read((char*)&NameLength, sizeof(unsigned));  // read the name length
-	name.resize(NameLength);
-	rf.read(&name[0], NameLength);  // read the name
-	//std::string name = tempName;
-	std::cout << name << std::endl;
-	if (std::filesystem::exists(name))
+
+	char* format = new char[4]; // deleted
+	rf.read(format, sizeof(char) * 4);  // read the encrypted format
+	format = (char*)RC4Enc::decrypt(format, password.c_str(), sizeof(char) * 4, password.size()); // decrypt the format // deleted
+	std::string p_name = "";
+	unsigned int p_fileNum = 1;
+	if (!strcmp(format, "dir "))
 	{
-		char answer;
-		std::cout << "\'" << name << "\'" << " is already exists. Overwrite? [y/n] ";
-		std::cin >> answer;
-		if (answer == 'y')
-		{
-			std::filesystem::remove(name); // remove the directory
-		}
-		else
-			std::cout << "Not overwriting - exists" << std::endl;
+		char* fileNum = new char[sizeof(unsigned)]; // deleted
+		rf.read(fileNum, sizeof(unsigned));  // read the encrypted name length
+		fileNum = (char*)RC4Enc::decrypt(fileNum, password.c_str(), sizeof(unsigned), password.size());
+		std::memcpy(&p_fileNum, fileNum, sizeof(unsigned));
+		char* NameLength = new char[sizeof(unsigned)];  // deleted
+		unsigned int p_NameLength;
+		rf.read(NameLength, sizeof(unsigned));  // read the encrypted name length
+		NameLength = (char*)RC4Enc::decrypt(NameLength, password.c_str(), sizeof(unsigned), password.size());
+		std::memcpy(&p_NameLength, NameLength, sizeof(unsigned));
+		char* name = new char[p_NameLength]; // deleted
+		rf.read(name, p_NameLength);  // read the name
+		p_name = RC4Enc::decrypt(name, password.c_str(), p_NameLength, password.size());
+		delete[] fileNum;
+		delete[] NameLength;
+		delete[] name;
 	}
+	
+	if (output != "")
+	{
+		p_name = output;
+	}
+	
+	if (!ifExist(p_name.c_str()))
+		goto clean;
 
 	if (!strcmp(format, "dir ")) // the encryption is of directory
 	{
-		std::filesystem::create_directory(name); // create the directory
-		std::cout << "decrypting directory" << std::endl;
+
+		std::filesystem::create_directory(p_name); // create the directory
+		p_name = p_name + "\\";
+	}
+	else if (!strcmp(format, "file"));
+	else
+	{
+		std::cout << "wrong encryption" << std::endl;
 		goto clean;
 	}
-	else if (strcmp(format, "file")) // the encryption is of file
+	for (unsigned i = 0; i < p_fileNum; i++)
 	{
-		std::ofstream wf(name, std::ios::binary); // open the output file to write
+		char* fileNameLength = new char[sizeof(unsigned)]; // deleted
+		unsigned int p_fileNameLength;
+		rf.read(fileNameLength, sizeof(unsigned));  // read the encrypted name length
+		fileNameLength = (char*)RC4Enc::decrypt(fileNameLength, password.c_str(), sizeof(unsigned), password.size());
+		std::memcpy(&p_fileNameLength, fileNameLength, sizeof(unsigned));
+		char* name = new char[p_fileNameLength]; // deleted
+		rf.read(name, p_fileNameLength);  // read the name
+		const char* p_fileName = (char*)RC4Enc::decrypt(name, password.c_str(), p_fileNameLength, password.size());
+		std::cout << p_name + p_fileName << std::endl;
+		std::ofstream wf(p_name + p_fileName, std::ios::binary); // open the output file to write
 		if (!wf) {
 			std::cout << "Cannot open file for writing!" << std::endl;
 			goto clean;
 		}
-		unsigned fileSize;
-		char byte;
-		rf.read((char*)&fileSize, sizeof(unsigned)); // read the file size
-		while (rf.read(&byte, 1))
+
+		char* fileSize = new char[sizeof(unsigned)]; // deleted
+		unsigned int p_fileSize;
+		rf.read(fileSize, sizeof(unsigned));
+		fileSize = (char*)RC4Enc::decrypt(fileSize, password.c_str(), sizeof(unsigned), password.size());
+		std::memcpy(&p_fileSize, fileSize, sizeof(unsigned));
+
+		unsigned times = p_fileSize / 500;
+		char* p_bytes;
+		char* bytes = new char[500]; // deleted
+		for (unsigned i = 0; i < times; i++)
 		{
-			byte++;
-			wf.write(&byte, 1);
+			rf.read(bytes, sizeof(char) * 500);
+			p_bytes = (char*)RC4Enc::decrypt(bytes, password.c_str(), sizeof(char) * 500, password.size());
+			wf.write(p_bytes, sizeof(char) * 500);
+			delete[] p_bytes;
 		}
+
+		unsigned remainingBytes = p_fileSize % 500;
+		if (remainingBytes > 0)
+		{
+			delete[] bytes; // delete the bytes for reuse
+			bytes = new char[remainingBytes]; // deleted
+			rf.read(bytes, sizeof(char) * remainingBytes);
+			p_bytes = (char*)RC4Enc::decrypt(bytes, password.c_str(), sizeof(char) * remainingBytes, password.size());
+			wf.write(p_bytes, sizeof(char) * remainingBytes);
+			delete[] p_bytes;
+		}
+		// cleanup:
 		wf.close();
-		rf.close();
-		goto clean;
+		delete[] fileNameLength;
+		delete[] name;
+		delete[] p_fileName;
+		delete[] fileSize;
+		delete[] bytes;
+
 	}
-	else // wrong encryption
-	{
-		std::cout << "wrong file encryption" << std::endl;
-		goto clean;
-	}
+	
 clean:
 
+	rf.close();
 	delete[] format;
+	
 	return;
 
 }
 #ifdef _DEBUG // if debug
+
 int main()
 {
+	// tests:
 	int argc = 5;
-	const char* argv[5] = { "encrypter", "-d", "encryptedImage", "lol", "hello_world2.png" };
+	//int argc = 4;
+	//const char* argv[5] = { "encrypter", "-e", "tests\\hello_world.png", "goodPass4356", "e_hello_world.png" };
+	//const char* argv[5] = { "encrypter", "-d", "e_hello_world.png", "goodPass4356", "hello_world.png" };
+	//const char* argv[5] = { "encrypter", "-ed", "tests", "goodPass4356", "encryptedDir" };
+	//const char* argv[4] = { "encrypter", "-ed", "tests", "goodPass4356"};
+	const char* argv[5] = { "encrypter", "-d", "encryptedtests", "goodPass4356", "tests2" };
+	//const char* argv[4] = { "encrypter", "-d", "e_hello_world.png", "goodPass4356"};
 #else 
 
 int main(int argc, char* argv[])
@@ -186,11 +306,15 @@ int main(int argc, char* argv[])
 #endif
 
 	if (argc == 1)
+	{
 		printHelp();
+		return -1;
+	}
+		
 	const char* output;
 	if (argc < 4)
 		return -1;
-	if (argc == 4)
+	else if (argc == 4)
 		output = "";
 	else
 		output = argv[4];
@@ -209,5 +333,6 @@ int main(int argc, char* argv[])
 		encryptFile(argv[2], output, argv[3], 3);
 	else
 		std::cout << "wrong parameter" << std::endl;
+	
 
 }
